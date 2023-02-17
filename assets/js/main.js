a2d45075e6ac9ebf215f311195a54cb869a8040a
ClaudioSUPERCODE <123948180+ClaudioSUPERCODE@users.noreply.github.com>
2023-02-17 14:23:09 +0100

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+// document.getElementById("info").innerHTML
+
+// info.innerHTML = "<h1>Hello World</h1>";
+// info.innerHTML += "<h2>how are you?</h2>";
+
+// document.getElementById('container').innerHTML
+
+// container.innerHTML = "<p>start of the element</p>"
+
+// document.write ("end of the element")
+
+container.innerHTML = "<figure><img src='https://unsplash.it/200/200' alt='random pic'/><figcaption>Fig.1</figcaption></figure>"
+container.innerHTML += "<figure><img src='https://unsplash.it/200/200' alt='random pic'/><figcaption>Fig.2</figcaption></figure>"
+container.innerHTML += "<figure><img src='https://unsplash.it/200/200' alt='random pic'/><figcaption>Fig.3</figcaption></figure>"
+
+container.style.display = "flex"
